5c7876e086a2fac1ec97d04fb39fda8ffb46bba1
Mohanprasath0510 <mohancoc3458@gmail.com>
2025-01-31 16:02:13 +0530

    extra details addup

--- 01_paramiko/02_bytes_unicode.py
+++ 01_paramiko/02_bytes_unicode.py
@@ -1,15 +1,15 @@
 #------------------convert byte to string---------------------
 
-b1= b"\n\n hi\tiam Mohan" #b1 is var name, And b""-given the data to byte format.
+b1= b"\n hi\tiam Mohan" #b1 is var name, And b""-given the data to byte format.
 print(b1)
 print(type(b1))
-print(b1.decode()) #decode(): convert to human readable format.
+print(b1.decode(),"\n") #decode(): convert to human readable format.
 
 
 
 #------------------convert string to byte---------------------
 
-intro_str = ''' 
+intro_str = '''
 Hello,
 My name is Mohan prasath, 
 and I am a Network Automation Developer with expertise in enterprise networking and security. 
@@ -18,9 +18,9 @@ I have 6 months of experience in network infrastructure, specializing in routing
 print(intro_str.encode()) #encode(): convert the output to bytes
 
 
-#------------------how to use emojis---------------------
+#------------------how to use emojis---------------------# emojis(byte/unicode) are available in "https://apps.timwhitlock.info/emoji/tables/unicode"
 
-byte_emoji = b"\xF0\x9F\x98\x89"
+byte_emoji = b"\n\xF0\x9F\x98\x89" #\n new line
 unicode_emoji = "\U0001F609" #"U+1F609": unicode default char is "8" but we have a "5"(U+ 1F609) char, so add "3"(000) char like as "\U0001F609"
 print(byte_emoji.decode())
 print(unicode_emoji)
